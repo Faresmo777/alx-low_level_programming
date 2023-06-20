b8ba9967addf3283930e7397b6d0eff6542a3d5e
Fares <faremm2005@gmail.com>
2023-06-20 09:30:21 -0700

    second try

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,17 @@
  */
 int print_last_digit(int n)
 {
-	int lastdigit;
+	int a;
 
 	if (n < 0)
 		n = -n;
 
-	lastdigit = n % 10;
+	a = n % 10;
 
-	_putchar(lastdigit + '0');
+	if (a < 0)
+		a = -a;
 
-	return (lastdigit);
+	_putchar(a + '0');
+
+	return (a);
 }
