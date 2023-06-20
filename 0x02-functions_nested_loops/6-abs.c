3092caf1697031037c30bf702658bca7a407e15e
Fares <faremm2005@gmail.com>
2023-06-20 08:49:15 -0700

    another try

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,15 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
+ *
+ * @n: input integer
+ *
+ * Return: the absolute value of n
  */
 int _abs(int n)
 {
-	if (n >= 0)
-	{
+	if (n < 0)
+		return (-n);
+	else
 		return (n);
-	}
-	return (-n);
 }
