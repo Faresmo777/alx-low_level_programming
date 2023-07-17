ccf071619627613a4697893095556d855af32fec
Fares <faremm2005@gmail.com>
2023-07-17 02:29:35 -0700

    fix 1

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints thename of the file
  *
  * Return: Always 0 (success)
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
