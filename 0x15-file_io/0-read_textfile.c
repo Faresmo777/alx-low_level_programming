93eaadf56d9ca0bddacf259e31007550e31fb0b8
Faresmo777 <faremm2005@gmail.com>
2023-10-30 08:06:46 -0700

    first task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * readAndPrintTextFile - Reads a text file and prints its content to STDOUT.
+ * @filename: The name of the text file to be read.
+ * @letters: The number of letters to be read from the file.
+ *
+ * Return: The actual number of bytes read and printed.
+ *         0 if the function fails or if the filename is NULL.
+ */
+ssize_t readAndPrintTextFile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	file_descriptor = open(filename, O_RDONLY);
+	if(file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	free(buffer);
+	close(file_descriptor);
+
+	return (bytes_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this is a read me file for the project 0x15-file_io
